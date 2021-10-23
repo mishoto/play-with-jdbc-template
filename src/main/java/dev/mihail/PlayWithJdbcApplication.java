96bfd5a651a9ac4fa7eec40de6b2c38daed0b2db
Mishoto <mihail.dev.io@gmail.com>
2021-10-23 09:00:03 +0300

    initial commit

---
+++ src/main/java/dev/mihail/PlayWithJdbcApplication.java
@@ -0,0 +1,13 @@
+package dev.mihail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWithJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWithJdbcApplication.class, args);
+	}
+
+}
